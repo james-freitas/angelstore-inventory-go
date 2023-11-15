3754bf55ca110215dd063f6098fe27f29a19ec35
James Freitas <jamesfrj@yahoo.com.br>
2023-11-15 15:50:25 -0300

    Add unit tests with mocks for category model

--- entity/category_test.go
+++ entity/category_test.go
@@ -1,21 +1,64 @@
 package entity
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
-// TestNewCategory is a unit test function for the NewCategory function.
+// MockCategoryRepository is a mock implementation of CategoryRepository for testing purposes.
+type MockCategoryRepository struct {
+	Categories []*Category
+}
+
+func (m *MockCategoryRepository) Create(category *Category) error {
+	m.Categories = append(m.Categories, category)
+	return nil
+}
+
+func (m *MockCategoryRepository) FindAll() ([]*Category, error) {
+	return m.Categories, nil
+}
+
 func TestNewCategory(t *testing.T) {
-	// Set up test data
-	name := "CategoryName"
-
-	// Call the function being tested
+	name := "TestCategory"
 	category := NewCategory(name)
 
-	// Assert the result
-	if category.Name != name {
-		t.Errorf("Expected %s, got %s", name, category.Name)
-	}
-	// Assert the category Id was assigned
-	if category.Id == "" {
-		t.Errorf("Expected non-nil Id, got nil")
-	}
+	// Ensure that id is generated
+	assert.NotEmpty(t, category.Id)
+
+	// Ensure that Name is set correctly
+	assert.Equal(t, name, category.Name)
+}
+
+func TestCategoryRepositoryCreate(t *testing.T) {
+	
+	// Setup
+	repo := &MockCategoryRepository{}
+	name := "TestCategory"
+	category := NewCategory(name)
+
+	// Test
+	err := repo.Create(category)
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Len(t, repo.Categories, 1)
+	assert.Equal(t, category, repo.Categories[0])	
+}
+
+func TestCategoryRepositoryFindAll(t *testing.T) {
+	
+	// Setup
+	repo := &MockCategoryRepository{}
+	name := "TestCategory"
+	category := NewCategory(name)
+	_ = repo.Create(category)
+
+	// Test
+	categories, err := repo.FindAll()
+
+	// Assert
+	assert.NoError(t, err)
+	assert.Len(t, categories, 1)
+	assert.Equal(t, category, categories[0])
 }
